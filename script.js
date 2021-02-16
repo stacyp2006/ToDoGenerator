306c7576d43cbc8d26195c9f5820aedb8a4a6513
Stacy Potten <stacylynnaz@gmail.com>
2021-02-16 11:49:51 -0700

    Add event listeners

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ const todoList = document.querySelector('.todo-list');
 const listTitle = document.querySelector('.list-title');
 const addItemBtn = document.querySelector('.new-todo-btn');
 
-// addListBtn.addEventListener('click', addList);
-// addItemBtn.addEventListener('click', addItem);
+addListBtn.addEventListener('click', addList);
+addItemBtn.addEventListener('click', addItem);
