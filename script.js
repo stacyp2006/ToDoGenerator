e89dc2855ca73b476eb8177a5da71a9abb482e72
Stacy Potten <stacylynnaz@gmail.com>
2021-02-16 15:09:42 -0700

    Create addToDo method

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 //Selectors
-const addListBtn = document.querySelector('.new-list-btn');
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
-const listTitle = document.querySelector('.list-title');
-const addItemBtn = document.querySelector('.new-todo-btn');
-const inputTitle = document.querySelector('.title-entry');
-const inputItem = document.querySelector('.todo-item');
 
 // Functions
-function addList() {}
+function addToDo(event) {
+  event.preventDefault();
+  const todoDiv = document.createElement('div');
+  todoDiv.classList.add('todo');
+  const newToDo = document.createElement('li');
+  newToDo.innerText = 'hey';
+  newToDo.classList.add('todo-item');
+  todoDiv.appendChild(newToDo);
+  const completedBtn = document.createElement('button');
+  completedBtn.innerText = 'Complete';
+  completedBtn.classList.add('complete-btn');
+  todoDiv.appendChild(completedBtn);
+  const trashBtn = document.createElement('button');
+  trashBtn.innerText = 'Trash';
+  trashBtn.classList.add('trash-btn');
+  todoDiv.appendChild(trashBtn);
+  todoList.appendChild(todoDiv);
+}
 
 //Event Listeners
-addListBtn.addEventListener('click', addList);
-// addItemBtn.addEventListener('click', addItem);
+todoButton.addEventListener('click', addToDo);
