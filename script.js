82b6dbbb7051d0025972ce7c3eedc9cf99cf0693
Stacy Potten <stacylynnaz@gmail.com>
2021-02-16 11:47:33 -0700

    Add query selectors

---
+++ script.js
@@ -0,0 +1,7 @@
+const addListBtn = document.querySelector('.new-list-btn');
+const todoList = document.querySelector('.todo-list');
+const listTitle = document.querySelector('.list-title');
+const addItemBtn = document.querySelector('.new-todo-btn');
+
+// addListBtn.addEventListener('click', addList);
+// addItemBtn.addEventListener('click', addItem);
