95b43b164a350918b4fa2e7fdf390f7219d4cfe7
Stacy Potten <stacylynnaz@gmail.com>
2021-02-16 14:10:07 -0700

    Add selectors for input fields

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
+//Selectors
 const addListBtn = document.querySelector('.new-list-btn');
 const todoList = document.querySelector('.todo-list');
 const listTitle = document.querySelector('.list-title');
 const addItemBtn = document.querySelector('.new-todo-btn');
+const inputTitle = document.querySelector('.title-entry');
+const inputItem = document.querySelector('.todo-item');
 
+// Functions
+function addList() {}
+
+//Event Listeners
 addListBtn.addEventListener('click', addList);
-addItemBtn.addEventListener('click', addItem);
+// addItemBtn.addEventListener('click', addItem);
